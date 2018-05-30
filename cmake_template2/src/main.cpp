76e3ba4dbde4d8b60b1eb36e5b32237edeece17d
taaheri <taaheri@outlook.com>
2018-05-30 09:34:03 +0430

    new edit

--- cmake_template2/src/main.cpp
+++ cmake_template2/src/main.cpp
@@ -1,28 +1,32 @@
+
 #include <iostream>
 #include <stdlib.h>
 #include "helper.h"
-
-int main()
+//#include "FileUtilities.h"
+int main(int argc, char *argv[])
 {
 
-   
     // printf("======================= TEST LIB SHARED (hello_test64.dll)\n");
     // Lib_Helper d;
     // d.add(120, 600);
     // printf("LIB HELPER : %d \n", d.getSum());
-    
+
     printf("\n======================= TEST EXE \n");
     int i = 1;
     int hh = 333;
     Helper *h = new Helper();
+    
     h->add(10, 21);
+    
     printf("HELPER RESULT---: %d\n", h->getSum());
 
     std::cout << "HELLO WORLD " << std::endl;
+    
     for (int k = 0; k < 5; k++)
     {
         printf(" v: %d ", k);
     }
     getchar();
+    
     return 0;
 }
